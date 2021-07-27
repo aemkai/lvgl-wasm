e5e4251a090003ae5bc69626185effb6a6ebe459
aemkai <87898467+aemkai@users.noreply.github.com>
2021-07-27 13:42:51 +0200

    Update jetbrains_mono_42.c

--- clock/jetbrains_mono_42.c
+++ clock/jetbrains_mono_42.c
@@ -5,7 +5,7 @@
  ******************************************************************************/
 
 #ifdef LV_LVGL_H_INCLUDE_SIMPLE
-#include "lvgl.h"
+#include "../lvgl.h"
 //#else
 //#include "lvgl/lvgl.h"
 #endif
